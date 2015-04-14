a56a56f47954f84ce212666e271571d0b1629cf5
Nikhil Vaze <nvaze@electric-cloud.com>
2015-04-14 14:52:56 -0700

    Remove build directory

--- build/resources/main/project/ec_setup.pl
+++
@@ -1,9 +0,0 @@
-# Data that drives the create step picker registration for this plugin.
-my %CreateBucket = ( 
-  label       => "EC-S3 - CreateBucket", 
-  procedure   => "CreateBucket", 
-  description => "", 
-  category    => "Resource Management" 
-);
-
-@::createStepPickerSteps = (\%CreateBucket);
--- build/resources/main/project/manifest.pl
+++
@@ -1,5 +0,0 @@
-@files = (
-	['//project/propertySheet/property[propertyName="ec_setup"]/value', 'ec_setup.pl'],
-	['//project/procedure[procedureName="CreateBucket"]/propertySheet/property[propertyName="ec_parameterForm"]/value', 'procedures/CreateBucket/form.xml'],
-	['//project/procedure[procedureName="CreateBucket"]/step[stepName="CreateBucket"]/command', 'procedures/CreateBucket/steps/CreateBucket.sh'],
-);
