b7d43712d5e4b9ea358cff5c7414e308bf694e42
Anand <anand@msys-tech.com>
2015-05-07 18:02:08 +0530

    Added Test framework in Java. Wrote first test class. TestCreateBucket

---
+++ src/test/java/ecplugins/s3/S3Util.java
@@ -0,0 +1,47 @@
+package ecplugins.s3;
+
+
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.*;
+import com.amazonaws.services.s3.transfer.*;
+;import java.util.Properties;
+
+/**
+ * Created by anand on 07-05-2015.
+ */
+public class S3Util {
+
+    public static void DeleteBucket(String bucketName) throws Exception {
+
+        Properties props = TestUtils.getProperties();
+
+        BasicAWSCredentials credentials = new BasicAWSCredentials(props.getProperty(StringConstants.ACCESS_ID), props.getProperty(StringConstants.SECRET_ACCESS_ID));
+
+// Create TransferManager
+        TransferManager  tx = new TransferManager(credentials);
+
+// Get S3 Client
+        AmazonS3 s3 = tx.getAmazonS3Client();
+
+        if (s3.doesBucketExist(bucketName)) {
+            s3.deleteBucket(bucketName);
+        }
+    }
+
+    public static boolean CheckIsBucketAvailable(String bucketName) throws Exception {
+
+        Properties props = TestUtils.getProperties();
+
+        BasicAWSCredentials credentials = new BasicAWSCredentials(props.getProperty(StringConstants.ACCESS_ID), props.getProperty(StringConstants.SECRET_ACCESS_ID));
+
+// Create TransferManager
+        TransferManager  tx = new TransferManager(credentials);
+
+// Get S3 Client
+        AmazonS3 s3 = tx.getAmazonS3Client();
+
+        return s3.doesBucketExist(bucketName);
+    }
+}
+
