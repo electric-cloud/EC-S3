5434a5a760412ed98a8c0858cb79fdd480a52bd1
Anand <anand@msys-tech.com>
2015-05-08 21:10:05 +0530

    Added Upload Object Test

---
+++ src/test/java/ecplugins/s3/UploadObjectTest.java
@@ -0,0 +1,70 @@
+package ecplugins.s3;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Properties;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+public class UploadObjectTest {
+
+    private static Properties props;
+
+    @BeforeClass
+    public static void setup() throws Exception {
+
+        props = TestUtils.getProperties();
+
+        TestUtils.deleteConfiguration();
+        TestUtils.createConfiguration();
+    }
+
+    @Test
+    public void UploadOneObject() throws Exception {
+        long jobTimeoutMillis = 5 * 60 * 1000;
+        String bucketName = "ec-s3-plugin-test-bucket" + TestUtils.randInt();
+        S3Util util = new S3Util();
+        if(!util.CheckIsBucketAvailable(bucketName))
+            util.CreateBucket(bucketName);
+
+        JSONObject jo = new JSONObject();
+
+        jo.put("projectName", "EC-S3-" + StringConstants.PLUGIN_VERSION);
+        jo.put("procedureName", "UploadObject");
+
+        JSONArray actualParameterArray = new JSONArray();
+        actualParameterArray.put(new JSONObject()
+                .put("value", bucketName)
+                .put("actualParameterName", "bucketName"));
+
+        actualParameterArray.put(new JSONObject()
+                .put("actualParameterName", "config")
+                .put("value", "S3Cfg"));
+
+        actualParameterArray.put(new JSONObject()
+                .put("actualParameterName", "fileToUpload")
+                .put("value", "/tmp/license.xml"));
+
+        actualParameterArray.put(new JSONObject()
+                .put("actualParameterName", "key")
+                .put("value", "template.xml"));
+
+
+        jo.put("actualParameter", actualParameterArray);
+
+        String jobId = TestUtils.callRunProcedure(jo);
+
+        String response = TestUtils.waitForJob(jobId, jobTimeoutMillis);
+
+        // Check job status
+        assertEquals("Job completed with errors", "success", response);
+
+    }
+
+
+}
