8a6c3f9e18e5c19fb01fdf86a5b4272c0a183a57
Nikhil Vaze <nvaze@electric-cloud.com>
2015-12-22 23:39:13 -0800

    Code changes to get tests to pass

--- src/test/java/ecplugins/s3/StringConstants.java
+++ src/test/java/ecplugins/s3/StringConstants.java
@@ -20,7 +20,7 @@ package ecplugins.s3;
 
 public class StringConstants {
 
-    final static String COMMANDER_SERVER = System.getProperty("COMMANDER_SERVER");
+    final static String COMMANDER_SERVER = "COMMANDER_SERVER";
     final static String PLUGIN_VERSION = System.getProperty("PLUGIN_VERSION");
     final static String COMMANDER_USER = "COMMANDER_USER";
     final static String COMMANDER_PASSWORD = "COMMANDER_PASSWORD";
